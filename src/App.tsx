a1121add29cd13d0c119adc696e686e59b7f48cd
corock510 <49363722+corock510@users.noreply.github.com>
2022-07-04 11:33:52 +0900

    初回コミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Radar } from "react-chartjs-2";
+
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+const chart = {
+  width: "40%",
+  display: "flex",
+};
+const data = {
+  labels: ["a", "b", "c", "d", "e", "f"],
+  datasets: [
+    {
+      label: "title",
+      data: [2, 2, 3, 3, 1, 2],
+      backgroundColor: "rgba(255, 99, 132, 0.2)",
+      borderColor: "rgba(255, 99, 132, 1)",
+      borderWidth: 2,
+    },
+  ],
+};
+
+const options = {
+  scales: {
+    r: {
+      min: 0,
+      max: 5,
+      stepSize: 1,
+    },
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={chart} className="chart">
+        <Radar data={data} options={options} />
+        <Radar data={data} />
+      </div>
+    </>
   );
 }
 
